9df6d02359a0864112bd5e19f23459d6bdf1a9ae
sugar99 <40078199+sugar99@users.noreply.github.com>
2019-05-10 22:41:01 +0800

    我在本地的InkYear仓库出了问题，只能这样更新main.dart

--- frontend_dev/lib/main.dart
+++ frontend_dev/lib/main.dart
@@ -1,28 +1,50 @@
 import 'package:flutter/material.dart';
+
 import 'package:frontend_dev/pages/state_page.dart';
 import 'package:frontend_dev/pages/drawer_page.dart';
 import 'package:frontend_dev/pages/weather_page.dart';
 import 'package:frontend_dev/tools/TopReminder.dart';
 import 'package:frontend_dev/tools/Toast.dart';
+import 'package:frontend_dev/tools/Swiper.dart';          //import swiper
+import 'package:frontend_dev/pages/snaplist_page.dart';   //import snaplist
 import 'package:frontend_dev/constants/ThemeColors.dart';
 import 'package:frontend_dev/constants/IconStyle.dart';
 import 'package:frontend_dev/constants/StringStyle.dart';
-import 'package:frontend_dev/datas/Location.dart'; // TODO:降低这个类的耦合
-import 'package:frontend_dev/tools/Card.dart'; //导入Card
+import 'package:frontend_dev/datas/Location.dart'; // TODO:降低类的耦合
+import 'package:frontend_dev/pages/card_page.dart';
+import 'package:frontend_dev/pages/card_packet_page.dart';
+import 'package:frontend_dev/pages/calendar_page.dart';
+
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:frontend_dev/database/database.dart';
+import 'package:frontend_dev/database/table_state.dart';
+import 'package:frontend_dev/database/table_config.dart';
+
+String databaseName = "InkYear"; // 数据库名字
+int year;
+int month;
+int day;
+bool isSetup = false; // 是否第一次使用
+bool isSameDay = false; // 是否同一天使用
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  MyApp() {
+    // TODO: 初始化数据库和数据表
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // 全局主题配置
       theme: ThemeData(
-        primaryColor: ThemeColors.primaryColor,
+        /*primaryColor: ThemeColors.primaryColor,
         backgroundColor: ThemeColors.backgroundColor,
         accentColor: ThemeColors.accentColor,
         primaryIconTheme: IconStyle.primaryIconStyle,
-        primaryTextTheme: StringStyle.primaryTextStyle,
+        primaryTextTheme: StringStyle.primaryTextStyle,*/
         // primaryTextTheme: ,
       ),
       // 入口界面控件
@@ -40,55 +62,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // 用户参数
-  // TODO:封装成User类
-  String _avatar = 'imgs/profile.jpeg'; // 头像
+  // TODO: 数据库重写
+  String _avatar = 'imgs/timg.jpg'; // 头像
   String _nickname = 'nickname'; // 昵称
   String _email = 'nickname@xxx.xxx'; // 邮箱
+  String _password = 'password'; // 密码
 
   // 日期参数
-  // TODO:用内置Date类
+  // TODO: 数据库重写
   String _month = "Jan";
   int _day = 1;
   String _date = "Jan 1";
   String _week = "MONDAY";
 
   // 页面参数
-  // TODO:在initState里初始化
   List<Widget> _bodys;
-  int _tabIndex = 2;
+  int _tabIndex = 1;
+  bool isEdit = true;
+  bool isCalendar = false;
 
-  // 得到页面
-  // TODO:临时填充将被替换
-  Widget _getBody(int index) {
-    return Center(
-      child: Text("body $index"),
-    );
-  }
+  static VoidCallback _openCardPacketCallback;
 
   @override
   void initState() {
     super.initState();
-    // TODO:写成生成器模式
     _bodys = [
-      _getBody(0),
+      CalenderPage(),
+
       //改卡片页面demo，还没写类
-      new Stack(
-        children: <Widget>[
-          new Positioned(
-            top: 50,
-            left: 40,
-            child: new MainCard(
-              cardSize: 110,
-              color: Colors.white,
-              child: new Center(
-                  child: new Text("Hello")
-              ),
-            ),
-          )
-        ],
-      ),
+      //CardPage(),         //TODO 更换这里
+      //SwiperPage(),
+      SnaplistPage(),
+
       DailyRecord(),
     ];
+
   }
 
   void _openTopReminder(context) {
@@ -97,149 +105,194 @@ class _MyHomePageState extends State<MyHomePage> {
         // 当前的路由不会遮盖之前的路由
         opaque: false,
         // 构建路由的主要内容
-        pageBuilder: (BuildContext context, _, __) => TopReminder(child:WeatherPage()),
+        pageBuilder: (BuildContext context, _, __) => TopReminder(child:WeatherPage(), reminderText: 'WeatherPage',),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    _openCardPacketCallback = (){
+      Navigator.push<String>(
+        context,
+        new MaterialPageRoute(builder: (context) => new CardPacketPage()),
+      ).then((String result){
+        Toast.toast(context, result);
+      });
+    };
+
     return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).backgroundColor,
-        image: DecorationImage(
-          image: AssetImage("imgs/background2.jpg"),
-          fit: BoxFit.fitWidth,
-        )
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          leading: Builder(
-            builder: (BuildContext context) =>
-              Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                    child: CircleAvatar(
-                      radius: 20.0,
-                      backgroundColor: Theme.of(context).backgroundColor,
-                      backgroundImage: AssetImage("imgs/timg.jpg"),
+        decoration: BoxDecoration(
+            color: Theme.of(context).backgroundColor,
+            image: DecorationImage(
+              image: AssetImage("imgs/background2.jpg"),
+              fit: BoxFit.fitWidth,
+            )
+        ),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: Builder(
+                builder: (BuildContext context) =>
+                    Stack(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                            child: CircleAvatar(
+                              radius: 20.0,
+                              backgroundColor: Theme.of(context).backgroundColor,
+                              backgroundImage: AssetImage("imgs/timg.jpg"),
+                            ),
+                          ),
+                          GestureDetector(
+                              onTap: () {
+                                Scaffold.of(context).openDrawer();
+                              }
+                          ),
+                        ]
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Scaffold.of(context).openDrawer();
-                    }
-                  ),
-                ]
               ),
-          ),
-          title: Text(
-            _nickname,
-            style: TextStyle(fontSize: 25),
-          ),
-          actions: <Widget>[
-            /// 显示日期
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    _date,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Padding(padding: EdgeInsets.fromLTRB(7, 0, 0, 0)),
-                  Text(
-                    _week,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
+              title: Text(
+                _nickname,
+                style: TextStyle(fontSize: 25),
               ),
-            ),
-            /// 显示天气
-            IconButton(
-              // TODO:动态变化天气图标
-                icon: Icon(
-                  Icons.cloud,
-                  color: Theme.of(context).accentColor,
+              actions: <Widget>[
+                /// 显示日期
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        _date,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Padding(padding: EdgeInsets.fromLTRB(7, 0, 0, 0)),
+                      Text(
+                        _week,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ),
-                onPressed: () {
-                  if(county == "") { // 未选择地址
-                    Toast.toast(context, "请先选择地址");
-                  }
-                  else { // 已选择地址
-                    _openTopReminder(context);
-                  }
-                }
+                /// 显示天气
+                IconButton(
+                  // TODO:动态变化图标
+                    icon: Icon(
+                      Icons.cloud,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    onPressed: () {
+                      if(county == "") { // 未选择地址
+                        Toast.toast(context, "请先选择地址");
+                      }
+                      else { // 已选择地址
+                        _openTopReminder(context);
+                      }
+                    }
+                ),
+                /// 显示搜索
+                IconButton(
+                  icon: Icon(Icons.search),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () async {
+                    // TODO: 初始化数据库和数据表
+                    await deleteDB(databaseName);
+                    await createDatabase(databaseName);
+                    await createStateTable();
+                    await addState();
+                    // TODO:实现搜索功能
+                    Toast.toast(context, "功能尚未开放......Orz\n敬请期待吧~(￣▽￣)");
+                  },
+                ),
+              ],
+              elevation: 0.0,
             ),
-            /// 显示搜索
-            IconButton(
-              icon: Icon(Icons.search),
-              color: Theme.of(context).accentColor,
-              onPressed: () {
-                // TODO:实现搜索功能
-              },
+            body: _bodys[_tabIndex],
+            bottomNavigationBar: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new IconButton(
+                  icon: new Icon(
+                    Icons.assignment,
+                    size: 35,
+                    color: _tabIndex == 0 ? Colors.blueAccent : Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _tabIndex = 0;
+                      isEdit = false;
+                      isCalendar = true;
+                    });
+                  },
+                ),
+                new IconButton(
+                  icon: new Icon(
+                    Icons.border_color,
+                    size: 35,
+                    color: _tabIndex == 1 ? Colors.blueAccent : Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _tabIndex = 1;
+                      isEdit = true;
+                      isCalendar = false;
+                    });
+                  },
+                ),
+                new IconButton(
+                  icon: new Icon(
+                    Icons.accessibility,
+                    size: 35,
+                    color: _tabIndex == 2 ? Colors.blueAccent : Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _tabIndex = 2;
+                      isEdit = false;
+                      isCalendar = false;
+                    });
+                  },
+                ),
+              ],
             ),
-          ],
-          elevation: 0.0,
-        ),
-        body: _bodys[_tabIndex],
-        bottomNavigationBar: new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            new IconButton(
+            drawer: DrawerPage(
+              oriAvatar: _avatar,
+              oriNickname: _nickname,
+              oriEmail: _email,
+              oriPassword: _password,
+            ),
+            floatingActionButton: isEdit ?
+            /*new Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 40, 20),
+          child: new CircleAvatar(
+            radius:30,
+            child: IconButton(
               icon: new Icon(
-                Icons.assignment,
-                size: 35,
-                color: _tabIndex == 0 ? Colors.blueAccent : Colors.white,
+                Icons.chrome_reader_mode,
+                size: 45,
+                color: Colors.white,
               ),
-              onPressed: () {
-                setState(() {
-                  _tabIndex = 0;
-                });
-              },
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+              onPressed: _openCardPacketCallback,
             ),
-            new IconButton(
-              icon: new Icon(
-                Icons.border_color,
-                size: 35,
-                color: _tabIndex == 1 ? Colors.blueAccent : Colors.white,
-              ),
-              onPressed: () {
-                setState(() {
-                  _tabIndex = 1;
-                });
-              },
-            ),
-            new IconButton(
-              icon: new Icon(
-                Icons.accessibility,
-                size: 35,
-                color: _tabIndex == 2 ? Colors.blueAccent : Colors.white,
-              ),
-              onPressed: () {
-                setState(() {
-                  _tabIndex = 2;
-                });
-              },
-            ),
-          ],
-        ),
-        drawer: DrawerPage(
-          oriAvatar: _avatar,
-          oriNickname: _nickname,
-          oriEmail: _email,
-        ),
-        floatingActionButton: IconButton(
-            icon: new Icon(
-              Icons.add_circle,
-              size: 40,
-            ),
-            onPressed: null
-        ),
-      )
+            backgroundColor: Colors.black54,
+          ),
+        )*/
+            new Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 40, 20),
+                child: FloatingActionButton(
+                  child: Icon(
+                    Icons.chrome_reader_mode,
+                    size: 45,
+                    color: Colors.white,
+                  ),
+                  onPressed: _openCardPacketCallback,
+                  backgroundColor: Colors.black54,
+                )
+            )
+                : null
+        )
     );
   }
 }
